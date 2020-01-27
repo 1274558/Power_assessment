a956e8a1af144fd512f74d1794b44f67ba84c31f
Susanna <susansavio13@gmail.com>
2020-01-27 21:02:15 +0100

    Handled service response for no image

--- src/components/EpisodeDetails/EpisodeDetails.js
+++ src/components/EpisodeDetails/EpisodeDetails.js
@@ -14,6 +14,12 @@ function EpisodeDetails({ match }) {
         const fetchItem = await fetch(`http://api.tvmaze.com/shows/${match.params.showid}/episodebynumber?season=${match.params.episodeseason}&number=${match.params.episodenumber}`);
         const episodeinformation = await fetchItem.json();
         const regexPattern = /(<([^>]+)>)/ig;
+        if(!episodeinformation.summary){
+            episodeinformation.summary = "Not Available."
+        }
+        if(!episodeinformation.image){
+            episodeinformation.image = {original : ""};
+        }
         const episodesummary = episodeinformation.summary.replace(regexPattern, '');
         episodeinformation.summary = episodesummary;
         setitem(episodeinformation);
@@ -24,7 +30,7 @@ function EpisodeDetails({ match }) {
         <div className="Episodecontainer container">
             <div className="row">
             <div className="Episodeimagecontainer col-xs-4 col-md-5 col-lg-4">
-                <img src={ episodeinformation.image.original } alt="Coverimage" />
+                <img src={ episodeinformation.image.original } alt="Coverimage not available" />
                 </div>
                 <div className="Episodeinfo col-xs-8 col-md-7 col-lg-8">
                     <h2>{episodeinformation.name}</h2>
